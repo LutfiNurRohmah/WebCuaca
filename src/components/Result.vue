c2f07cf1b105ba1ba34a9f370d6ce88f834297d3
solehudin5699 <solehudinamqi@yahoo.co.id>
2020-12-28 17:44:16 +0700

    add countryID

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -10,7 +10,7 @@
       <SpinnerLoader color="rgb(0, 119, 255)" />
     </div>
     <div class="result_content" v-if="data.weather && !data.isLoading">
-      <h3 class="result_nameCity">{{ data.name }}</h3>
+      <h3 class="result_nameCity">{{ data.name }}, {{ data.sys.country }}</h3>
       <img
         :src="`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`"
       />
@@ -68,85 +68,85 @@
 </template>
 
 <script>
-  import { FacebookLoader, SpinnerLoader } from "vue-spinners-css";
-  export default {
-    name: "Result",
-    props: ["data"],
-    data() {
-      return {
-        condition: {
-          Thunderstorm: "Hujan badai",
-          Drizzle: "Gerimis",
-          Rain: "Hujan",
-          Snow: "Bersalju",
-          Clear: "Cerah",
-          Clouds: "Berawan",
-          Mist: "Berkabut",
-          Smoke: "Berasap",
-          Haze: "Berkabut",
-          Dust: "Berdebu",
-          Fog: "Berkabut",
-          Sand: "Berpasir",
-          Ash: "Berabu",
-          Squall: "Badai",
-          Tornado: "Tornado",
-        },
-      };
-    },
-  };
+import { FacebookLoader, SpinnerLoader } from "vue-spinners-css";
+export default {
+  name: "Result",
+  props: ["data"],
+  data() {
+    return {
+      condition: {
+        Thunderstorm: "Hujan badai",
+        Drizzle: "Gerimis",
+        Rain: "Hujan",
+        Snow: "Bersalju",
+        Clear: "Cerah",
+        Clouds: "Berawan",
+        Mist: "Berkabut",
+        Smoke: "Berasap",
+        Haze: "Berkabut",
+        Dust: "Berdebu",
+        Fog: "Berkabut",
+        Sand: "Berpasir",
+        Ash: "Berabu",
+        Squall: "Badai",
+        Tornado: "Tornado",
+      },
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .result_container_loading {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-    height: 65vh;
-    padding-top: 15px;
-  }
-  .result_container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex: 1;
-    padding-top: 15px;
-    padding-bottom: 15px;
-  }
+.result_container_loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  height: 65vh;
+  padding-top: 15px;
+}
+.result_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding-top: 15px;
+  padding-bottom: 15px;
+}
+.result_content {
+  background-color: rgba(0, 119, 255, 0.5);
+  border-radius: 10px;
+  padding: 15px;
+  color: #ffffff;
+  min-width: 50%;
+}
+@media screen and (max-width: 768px) {
   .result_content {
-    background-color: rgba(0, 119, 255, 0.5);
-    border-radius: 10px;
-    padding: 15px;
-    color: #ffffff;
-    min-width: 50%;
+    min-width: 90%;
   }
-  @media screen and (max-width: 768px) {
-    .result_content {
-      min-width: 90%;
-    }
-  }
-  .result_nameCity {
-    text-align: center;
-  }
-  .result_notfound {
-    color: rgb(29, 80, 138);
-  }
-  .result_description {
-    font-style: italic;
-    color: rgba(252, 248, 248, 0.829);
-    margin-top: -5px;
-  }
-  .result_table {
-    width: 100%;
-  }
-  .result_tableRow {
-    color: #ffffff;
-  }
-  .tableValue {
-    padding-left: 10px;
-  }
-  .result_tempAverage {
-    position: relative;
-  }
+}
+.result_nameCity {
+  text-align: center;
+}
+.result_notfound {
+  color: rgb(29, 80, 138);
+}
+.result_description {
+  font-style: italic;
+  color: rgba(252, 248, 248, 0.829);
+  margin-top: -5px;
+}
+.result_table {
+  width: 100%;
+}
+.result_tableRow {
+  color: #ffffff;
+}
+.tableValue {
+  padding-left: 10px;
+}
+.result_tempAverage {
+  position: relative;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,126 +23,121 @@
 </template>
 
 <style scoped>
-  .home {
-    padding: 0;
-    margin: 0;
-    width: 100%;
+.home {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+.search_container {
+  background-color: rgb(0, 119, 255);
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 35vh;
+}
+.search_logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.search_logo > img {
+  height: 35vh;
+  margin-right: 15px;
+}
+@media screen and (max-width: 768px) {
+  .search_logo > img {
+    height: 15vh;
   }
   .search_container {
-    background-color: rgb(0, 119, 255);
-    padding: 0;
-    margin: 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    height: 35vh;
-    /* z-index: 10;
-    position: sticky; */
-  }
-  .search_logo {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-direction: column;
+    height: 30vh;
   }
   .search_logo > img {
-    height: 35vh;
-    margin-right: 15px;
+    margin-right: 0;
   }
-  @media screen and (max-width: 768px) {
-    .search_logo > img {
-      height: 15vh;
-    }
-    .search_container {
-      flex-direction: column;
-      height: 30vh;
-    }
-    .search_logo > img {
-      margin-right: 0;
-    }
-  }
-  .search_logo > h3 {
-    font-size: 17px;
-  }
-  .search_inputContainer {
-    flex: 0.6;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    /* padding-left: 10px;
-    margin-left: 35px; */
-  }
-  .input_wrap {
-    width: 100%;
-    height: 45px;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: row;
-    background-color: #ffffff;
-    justify-content: space-between;
-  }
-  .input_search {
-    flex: 0.9;
-    /* width: 100%; */
-    height: 45px;
-    border-radius: 10px;
-    border: none;
-    outline: none;
-    padding-left: 15px;
-    padding-right: 5px;
-    font-size: 15px;
-  }
-  .input_search:focus {
-    font-size: 15px;
-  }
-  .input_search::placeholder {
-    font-size: 15px;
-    color: rgba(0, 119, 255, 0.7);
-    font-weight: 500;
-  }
-  .input_btn {
-    flex: 0.1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: rgba(0, 119, 255, 0.3);
-    border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
-    min-width: 45px;
-    cursor: pointer;
-  }
+}
+.search_logo > h3 {
+  font-size: 17px;
+}
+.search_inputContainer {
+  flex: 0.6;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.input_wrap {
+  width: 100%;
+  height: 45px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  background-color: #ffffff;
+  justify-content: space-between;
+}
+.input_search {
+  flex: 0.9;
+  height: 45px;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  padding-left: 15px;
+  padding-right: 5px;
+  font-size: 15px;
+}
+.input_search:focus {
+  font-size: 15px;
+}
+.input_search::placeholder {
+  font-size: 15px;
+  color: rgba(0, 119, 255, 0.7);
+  font-weight: 500;
+}
+.input_btn {
+  flex: 0.1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 119, 255, 0.3);
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  min-width: 45px;
+  cursor: pointer;
+}
 </style>
 
 <script>
-  // @ is an alias to /src
-  import Result from "@/components/Result.vue";
+// @ is an alias to /src
+import Result from "@/components/Result.vue";
 
-  export default {
-    name: "Home",
-    components: {
-      Result,
+export default {
+  name: "Home",
+  components: {
+    Result,
+  },
+  data() {
+    return {
+      keyword: "",
+      data: { isLoading: false },
+      error: "",
+      baseURL: "https://api.openweathermap.org",
+      apiKey: process.env.VUE_APP_APIKEY,
+    };
+  },
+  methods: {
+    searchAction() {
+      this.data = { ...this.data, isLoading: true };
+      fetch(
+        `${this.baseURL}/data/2.5/weather?q=${this.keyword}&appid=${this.apiKey}&units=metric&lang=id`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          this.data = { ...data, isLoading: false };
+        })
+        .catch((err) => (this.error = err));
     },
-    data() {
-      return {
-        keyword: "",
-        data: { isLoading: false },
-        error: "",
-        baseURL: "https://api.openweathermap.org",
-        apiKey: process.env.VUE_APP_APIKEY,
-      };
-    },
-    methods: {
-      searchAction() {
-        this.data = { ...this.data, isLoading: true };
-        fetch(
-          `${this.baseURL}/data/2.5/weather?q=${this.keyword}&appid=${this.apiKey}&units=metric&lang=id`
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            this.data = { ...data, isLoading: false };
-          })
-          .catch((err) => (this.error = err));
-      },
-    },
-  };
+  },
+};
 </script>
